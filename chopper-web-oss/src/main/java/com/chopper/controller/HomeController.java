de0f06424533faa8d4ebf6f05f00f5d88d77fb42
wangqb <wangqiaobo@lyancoffee.com>
2017-09-14 17:52:52 +0800

    增加spring依賴，以及springmvc的配置

---
+++ chopper-web-oss/src/main/java/com/chopper/controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.chopper.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by chopper on 2017/9/14.
+ */
+@Controller
+public class HomeController {
+
+    @RequestMapping(name = "home", method = RequestMethod.GET)
+    public String home(){
+        return "home";
+    }
+}
