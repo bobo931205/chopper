3ed9b186a421ef8a4ce1c24d5f57965c24ffbc1b
wangqb <326187106@qq.com>
2017-10-08 13:10:59 +0800

    加入bootstrap框架 新增个人履历项目

--- curriculum-vitae/src/main/java/com/chopper/cv/controller/CvController.java
+++ curriculum-vitae/src/main/java/com/chopper/cv/controller/CvController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class CvController {
 
 
-    @RequestMapping(value= "index", method = RequestMethod.GET)
+    @RequestMapping(value= "/", method = RequestMethod.GET)
     public String home(){
 
         return "index";
